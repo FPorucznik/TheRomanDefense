f42e6130d428ce9e59057e8f2947505ea3e7202a
FPorucznik <porucznik199@wp.pl>
2022-06-01 03:05:47 +0200

    added fix wall upgrade

---
+++ TheRomanDefense/Assets/Scripts/Upgrades.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrades : MonoBehaviour
+{
+    private Base baseObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        baseObj = FindObjectOfType<Base>();
+    }
+
+    public void FixWall()
+    {
+        if(baseObj.gold >= 10)
+        {
+            baseObj.health = 100;
+            baseObj.gold -= 10;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
